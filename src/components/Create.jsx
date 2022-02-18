124465d01535b3c8ae4b39788669735f87330d4c
Fergieboi <90807420+Fergieboi@users.noreply.github.com>
2022-02-19 08:38:01 +1300

    add links and un styled buttons to work routing

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,10 +1,15 @@
 import React from "react"
+import {Link} from 'react-router-dom'
 
 const Create = () => {
 
   return (
     <div>
-      <h1>Create</h1>
+      <Link to={`event`}>
+        <button>
+          Next
+        </button>
+      </Link>
     </div>
   )
 }
